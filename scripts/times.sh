612850ea5cbb634ccb427ff1eed1a2eef7314141
Renan M. Gomes <renangomes.es@gmail.com>
2019-07-04 23:56:47 -0300

    Coisas para fazer relatorio

--- scripts/times.sh
+++ scripts/times.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+.././indexador lista ../textos/*.txt > ../benchs/lista.txt;
+.././indexador arvore ../textos/*.txt > ../benchs/arvore.txt;
+.././indexador avl ../textos/*.txt > ../benchs/avl.txt;
+.././indexador trie ../textos/*.txt > ../benchs/trie.txt;
+.././indexador hash ../textos/*.txt > ../benchs/hash.txt;
